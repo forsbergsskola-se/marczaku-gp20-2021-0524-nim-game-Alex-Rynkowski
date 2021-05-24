a1cb38f131e171cadea2cd01d140ad59c283d950
Alex <Alex.rynkowski90@gmail.com>
2021-05-24 19:40:06 +0200

    update gitignore + add visualisation

--- TicTacToe/TicTacToe.cpp
+++ TicTacToe/TicTacToe.cpp
@@ -1,6 +1,20 @@
+#include <iostream>
 
-int main(int argc, char* argv[])
-{
-    
+using namespace std;
+
+void Visualise(){
+    for (int i = 0; i < 9; i++){
+        cout << " " << i << " ";
+        if (i == 2 || i == 5 || i == 8){
+            cout << "" << endl;
+            continue;
+        }
+        cout << "|";
+    }
+}
+
+int main(int argc, char* argv[]){
+
+    Visualise();
     return 0;
 }
