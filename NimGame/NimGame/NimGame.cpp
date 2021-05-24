8571326d552ded53e8eddbef8daae6530f8ddc02
Alex <Alex.rynkowski90@gmail.com>
2021-05-24 17:41:04 +0200

    creates nim project

---
+++ NimGame/NimGame/NimGame.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+class Program{
+    int main(int argc, char* argv[]){
+        int matches = 24;
+        while (matches > 0){
+
+        }
+
+        return 0;
+    }
+    void AITurn(int matchesLeft){
+        
+    }
+};
