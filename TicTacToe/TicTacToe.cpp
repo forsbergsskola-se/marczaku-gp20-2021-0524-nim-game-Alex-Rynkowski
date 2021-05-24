06d360d6c6b8323b6c563b30f0692708a02f9ae5
Alex <Alex.rynkowski90@gmail.com>
2021-05-24 20:26:47 +0200

    implement read input and visualise it

--- TicTacToe/TicTacToe.cpp
+++ TicTacToe/TicTacToe.cpp
@@ -2,9 +2,27 @@
 
 using namespace std;
 
-void Visualise(){
+bool VisualisePlayerOne(int playerOne[], int i){
+    return playerOne[i] > 0;
+}
+
+bool VisualisePlayerTwo(int playerTwo[], int i){
+    return playerTwo[i] > 0;
+}
+
+void Visualise(int playerOne[], int playerTwo[]){
     for (int i = 0; i < 9; i++){
-        cout << " " << i << " ";
+        if (VisualisePlayerOne(playerOne, i)){
+            cout << " " << "x" << " ";
+        }
+
+        else if (VisualisePlayerTwo(playerTwo, i)){
+            cout << " " << "o" << " ";
+        }
+        else{
+            cout << " " << i << " ";
+        }
+
         if (i == 2 || i == 5 || i == 8){
             cout << "" << endl;
             continue;
@@ -13,8 +31,24 @@ void Visualise(){
     }
 }
 
-int main(int argc, char* argv[]){
+bool IsAvailableSlot(int playerOne[], int playerTwo[], int playerInput){
+    return playerOne[playerInput] == 0 && playerTwo[playerInput] == 0;
+}
 
-    Visualise();
+int main(int argc, char* argv[]){
+    int playerOne[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+    int playerTwo[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+    while (true){
+        cout << "Player one, it's your turn" << endl;
+        int inputOne;
+        cin >> inputOne;
+        playerOne[inputOne] = 1;
+        Visualise(playerOne, playerTwo);
+        cout << "Player two, it's your turn" << endl;
+        int inputTwo;
+        cin >> inputTwo;
+        playerTwo[inputTwo] = 2;
+        Visualise(playerOne, playerTwo);
+    }
     return 0;
 }
