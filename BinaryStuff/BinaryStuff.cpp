410b6da789e96c2b7e88b90fbe38b25b605a0b81
Alex <Alex.rynkowski90@gmail.com>
2021-05-27 12:06:52 +0200

    convert to binary using binary opretaions

--- BinaryStuff/BinaryStuff.cpp
+++ BinaryStuff/BinaryStuff.cpp
@@ -1,20 +1,39 @@
 #include <iostream>
+#include <list>
 #include <string>
+#include <vector>
 
 using namespace std;
 
-int main(int argc, char* argv[]){
-    signed int userInput;
+void RemainderBinaryConverter(){
+    unsigned int userInput;
     cin >> userInput;
-
     string result;
-    int remainder;
     while (userInput > 0){
-        remainder = userInput % 2;
+        int remainder = userInput % 2;
         userInput /=2;
         result = std::to_string(remainder) + result;
     }
-
+    
     cout << result << endl;
+}
+
+void BinaryOperationsBinaryConverter(){
+    signed int userInput;
+    list<int> num;
+    cin >> userInput;
+    
+    while (userInput){
+        num.push_front(userInput & 1);
+        userInput >>= 1;
+    }
+
+    for (int x : num){
+        cout << x;
+    }
+}
+
+int main(int argc, char* argv[]){
+    BinaryOperationsBinaryConverter();
     return 0;
 }
